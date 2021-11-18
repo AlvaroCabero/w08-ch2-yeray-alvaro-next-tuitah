d9e86c74ada58b3029d5c6b04e90acf367bd4b45
ysarmiento <yeray.sarmiento@hotmail.com>
2021-11-18 21:52:29 +0100

    Add grid style

--- components/Tuit/Tuit.js
+++ components/Tuit/Tuit.js
@@ -1,10 +1,14 @@
+import style from "../../styles/Feed.module.css";
+
 const Tuit = ({ tuit }) => (
   <>
-    <li className="">
-      <h4 className="">{tuit.text}</h4>
-      <p className="">{tuit.date}</p>
-      <button>{tuit.likes}</button>
-      <button>x</button>
+    <li className={style.square}>
+      <div className={style.infobar}>
+        <button className={style.button__likes}>{tuit.likes}</button>
+        <button className={style.button__delete}>x</button>
+      </div>
+      <h4 className={style.text}>{tuit.text}</h4>
+      <p className={style.date}>{tuit.date}</p>
     </li>
   </>
 );
--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,17 @@
 import "../styles/global.css";
 import Head from "next/head";
 import Header from "../components/Header/Header";
+import style from "../styles/App.module.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <div className={style.app}>
       <Head>
         <title>A/Y Tuitah</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
