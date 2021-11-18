37ddb6c27c270d6d48a6c07e92a17abdcdb0762a
ysarmiento <yeray.sarmiento@hotmail.com>
2021-11-18 22:09:03 +0100

    Add delete functionality

--- components/Tuit/Tuit.js
+++ components/Tuit/Tuit.js
@@ -1,16 +1,36 @@
+import "@fontsource/roboto";
+import axios from "axios";
 import style from "../../styles/Feed.module.css";
 
-const Tuit = ({ tuit }) => (
-  <>
-    <li className={style.square}>
-      <div className={style.infobar}>
-        <button className={style.button__likes}>{tuit.likes}</button>
-        <button className={style.button__delete}>x</button>
-      </div>
-      <h4 className={style.text}>{tuit.text}</h4>
-      <p className={style.date}>{tuit.date}</p>
-    </li>
-  </>
-);
+const Tuit = ({ tuit }) => {
+  const onDelete = async (id) => {
+    console.log(id);
+    try {
+      await axios.delete(
+        `https://w08ch2-yerayalvaro-next-tuitah.herokuapp.com/tuits/delete/${id}`
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <li className={style.square}>
+        <div className={style.infobar}>
+          <button className={style.button__likes}>{tuit.likes}</button>
+          <button
+            onClick={() => onDelete(tuit.id)}
+            className={style.button__delete}
+          >
+            x
+          </button>
+        </div>
+        <h4 className={style.text}>{tuit.text}</h4>
+        <p className={style.date}>{tuit.date}</p>
+      </li>
+    </>
+  );
+};
 
 export default Tuit;
